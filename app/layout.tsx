22138b77840589e6fca8fa20e308fb7534998985
ToasterMcStrudel <ryan.j.ortuno@gmail.com>
2023-06-18 16:29:34 -0600

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import './styles/customColors.scss'
+import './globals.scss'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+const nextConfig = {
+    reactStrictMode:true,
+    includePaths: [path.join(__dirname, 'styles')],
+}
 
 module.exports = nextConfig
