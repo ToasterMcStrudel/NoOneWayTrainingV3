221039768b7f144a73b0a6652f6a89174df699a3
ToasterMcStrudel <ryan.j.ortuno@gmail.com>
2023-06-18 16:36:59 -0600

    Custom color check

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-primary'>Hello World</div>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col p-0'>
+          <div className='text-primary bg-light'>Hello World</div>
+        </div>
+      </div>
+    </div>
   )
 }
